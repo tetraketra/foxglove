5d89f0fb9e2dcc339e1c70ec59cbc2dfd1d04451
Tetra Silverberg <tetra.silv@gmail.com>
2023-03-07 13:57:45 -0500

    trimmed trailing spaces and reduced line width

--- scripts/bouquet.py
+++ scripts/bouquet.py
@@ -37,18 +37,18 @@ class frame:
     - `name: str`, the name of the frame. Only for convenience; does nothing internally.
     - `data: np.ndarray`, the frame's data in U1 array format.
     - `config: dict`, the optional frame config options. `fixed_size` is determined automatically.
-    """    
+    """
     name: str
     data: np.ndarray
     config: dict
     regions: region = field(init = False, repr = True) #auto-generated
     region_sort_order: tuple = ("type", "pos", "dims") #subset any order
-    
+
     def __post_init__(self):
         symbols_in_frame = np.intersect1d(RESERVED_TYPE_SYMBOLS, self.data)
         self.regions = collapse([_all_regions_of_type_from_frame_data(self.data, type) for type in symbols_in_frame])
         self.regions = sorted(self.regions, key = lambda x: itemgetter(*self.region_sort_order)(asdict(x)))
-        
+
         self.config = {**self.config, "fixed_size":(not np.intersect1d(RESERVED_EXPANSION_SYMBOLS, self.data))}
 
 
@@ -64,10 +64,11 @@ def file_to_frames(file_path: str) -> list[frame]:
     Returns:
     - `frame_list: list[frame]`, a list of frame objects.
     """
-    
+
     bqt_file = (line for line in map(lambda x: x.strip("\n\t"), fileinput.input(file_path)) if line != "")
     chunks = ([*split_at(c, lambda x: x == "CONFIG")] for c in split_at(bqt_file, lambda x: x == "END") if c != [])
     return [frame(c[0][0][6:], _strlist_to_ndarray(c[0][1:]), _strlist_to_dict(c[1]) if len(c) >= 2 else {}) for c in chunks]
+        # `len(c) >= 2`` accounts for config-less frames.
 
 
 
@@ -82,23 +83,23 @@ def _all_regions_of_type_from_frame_data(data: np.ndarray, type: str) -> list[re
 
     Returns:
     - `region_list: list[region]`, the list of regions found in the frame.data of a particular type.
-    """    
+    """
 
     regions, coords_in_regions = [], []
 
     while True:
         reg_wspan, reg_hspan = 0, 0
-        reg_start = ()
-        
+        reg_pos = ()
+
         for iy, ix in np.ndindex(data.shape):
             in_existing_region = (iy, ix) in coords_in_regions
 
-            if not reg_start and data[iy, ix] == type and not in_existing_region:
-                reg_start = (iy, ix)
-            
-            elif reg_start and (data[iy, ix] != type or ix + 1 == data.shape[1]) and not in_existing_region:
-                reg_wspan = ix - reg_start[1] + int(ix + 1 == data.shape[1])
-                
+            if not reg_pos and data[iy, ix] == type and not in_existing_region:
+                reg_pos = (iy, ix)
+
+            elif reg_pos and (data[iy, ix] != type or ix + 1 == data.shape[1]) and not in_existing_region:
+                reg_wspan = ix - reg_pos[1] + int(ix + 1 == data.shape[1])
+
                 while reg_hspan := reg_hspan + 1:
                     if iy + 1 >= data.shape[0]: break
                     if data[iy := iy + 1, ix - 1] != type: break
@@ -106,8 +107,10 @@ def _all_regions_of_type_from_frame_data(data: np.ndarray, type: str) -> list[re
         else:
             break
 
-        coords_in_regions.extend((coord[0] + reg_start[0], coord[1] + reg_start[1]) for coord in np.ndindex(reg_hspan, reg_wspan))
-        regions.append(region(type, reg_start, (reg_hspan, reg_wspan)))
+        reg_coords = ((reg_pos[0] + coord[0], reg_pos[1] + coord[1]) for coord in np.ndindex(reg_hspan, reg_wspan))
+        coords_in_regions.extend(reg_coords)
+
+        regions.append(region(type, reg_pos, (reg_hspan, reg_wspan)))
 
     return regions
 
